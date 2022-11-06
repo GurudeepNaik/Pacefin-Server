57b11f7053265d7d7e4adab24ebccaf9436b6a7e
GurudeepNaik <gurudeepnaik1999@gmail.com>
2022-11-06 17:17:16 +0530

    Created Post Route for Login User

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const route = require("./routes/routes");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7000;
 
 
 mongoose.connect('mongodb://localhost:27017/news-api-app',(err) => {
@@ -13,6 +14,8 @@ mongoose.connect('mongodb://localhost:27017/news-api-app',(err) => {
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", route);
+
 
 app.get("*", (req, res) => {
   res.status(404).json({
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const user = require("../model/userModel");
 const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const secret = "SECRET";
 
-router.post("/register", body("password"), body("email").isEmail(), body("username"), async (req, res) => {
+router.post(
+  "/register",
+  body("password"),
+  body("email").isEmail(),
+  body("username"),
+  async (req, res) => {
     try {
       const repeatedEmail = await user.find({ email: req.body.email });
       if (repeatedEmail.length === 0) {
@@ -18,7 +25,7 @@ router.post("/register", body("password"), body("email").isEmail(), body("userna
           const { password, username, email } = req.body;
           const salt = await bcrypt.genSalt(12);
           bcrypt.hash(password, salt, async (err, hash) => {
-            await data.create({
+            await user.create({
               email: email,
               password: hash,
               username: username,
@@ -39,6 +46,77 @@ router.post("/register", body("password"), body("email").isEmail(), body("userna
       res.status(500).json({
         status: "Failed",
         message: err.message,
+      });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  body("email").isEmail(),
+  body("username"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({
+          status: "Failed By Validator",
+          message: errors.array(),
+        });
+      } else {
+        if (req.body.isGoogleLoggedIn) {
+          const { email, username } = req.body;
+          const isUserhasAccount = await user.findOne({ email: email });
+          if (isUserhasAccount === null) {
+            await user.create({
+              email: email,
+              username: username,
+            });
+          }
+          const userData = await user.findOne({ email: email });
+          const token = jwt.sign(
+            { exp: Math.floor(Date.now() / 10) + 60 * 60, data: userData._id },
+            secret
+          );
+          res.status(200).json({
+            Status: "Successful",
+            token: token,
+          });
+        } else {
+          const { email, password } = req.body;
+          const userData = await user.findOne({ email: email });
+          if (userData != null) {
+            const result = await bcrypt.compare(password, userData.password);
+            if (result) {
+              const token = jwt.sign(
+                {
+                  exp: Math.floor(Date.now() / 10) + 60 * 60,
+                  data: userData._id,
+                },
+                secret
+              );
+              res.status(200).json({
+                Status: "Successful",
+                token: token,
+              });
+            } else {
+              res.status(400).json({
+                status: "failed",
+                message: "Wrong Password",
+              });
+            }
+          } else {
+            res.status(400).json({
+              status: "failed",
+              message: "No user Found",
+            });
+          }
+        }
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "Failed",
+        message: error.message,
       });
     }
   }
