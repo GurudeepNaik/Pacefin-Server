ec9a3c20135fdf34e82084c72495dd4a3581ace3
GurudeepNaik <gurudeepnaik1999@gmail.com>
2022-11-06 13:58:15 +0530

    Innitialized Basic App

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://localhost:27017/news-api-app',(err) => {
+    if (err) console.log(err);
+    else console.log("Database Connected");
+  }
+);
+
+app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: "Failed",
+    message: "Invalid Path",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up at http://localhost:${PORT}/`);
+});
